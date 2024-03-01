72e4a1d33fb9b43e214998432b8e9f4d1644da29
gokhankaya <gokhankaya48@icloud.com>
2024-03-01 22:13:31 +0300

    gökhan11

---
+++ src/Gokhan1.java
@@ -0,0 +1,9 @@
+import Utility.BaseDriver;
+import org.junit.Test;
+
+public class Gokhan1 extends BaseDriver {
+    @Test
+    public void test(){
+        System.out.println("true = " + true);
+    }
+}
