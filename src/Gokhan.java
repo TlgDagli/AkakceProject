aeefbc9ed8bd36147169bd18a98232049fdd2147
tdagl <tolgamoon@gmail.com>
2024-03-01 22:15:31 +0300

    hesap acıldı

---
+++ src/Gokhan.java
@@ -0,0 +1,11 @@
+import Utility.BaseDriver;
+import org.junit.Test;
+
+public class Gokhan extends BaseDriver {
+    @Test
+    public void test(){
+        System.out.println("true = " + true);
+
+    }
+
+}
