9be0983a8882255870f722b686111c460d986ddf
Ali kemal çakıral <107760881+mavrosispira@users.noreply.github.com>
2024-03-05 00:58:15 +0300

    ali

--- src/AliKemal.java
+++ src/AliKemal.java
@@ -1,5 +1,9 @@
 import Utility.BaseDriver;
+import org.junit.Test;
 
 public class AliKemal extends BaseDriver{
-
+    @Test
+    public void test1() {
+        driver.get("https://www.akakce.com/");
+    }
 }
